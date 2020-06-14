fc05caa28c9b7ae46c303ad8ff534ad12e8d6cbd
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-14 13:31:28 -0300

    Add User validations and change token request method to find user by login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
 
   has_many :materials
   has_many :recipes
+
+  validates :name, :login, :email, presence: true
+  validates :login, :email, uniqueness: true
+
+  def self.from_token_request request
+    # Returns a valid user, `nil` or raise `Knock.not_found_exception_class_name`
+    login = request.params["auth"] && request.params["auth"]["login"]
+    self.find_by login: login
+  end
 end
