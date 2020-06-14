706c09757dbb6b1a8572ed211fbf983b7d6e52e3
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-13 22:21:35 -0300

    Run migrations and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_233310) do
+ActiveRecord::Schema.define(version: 2020_06_14_011444) do
 
   create_table "materials", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2020_05_07_233310) do
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.text "instructions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "login"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
