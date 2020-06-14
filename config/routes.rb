04c6dad93abd9b36f4a806b29d5dd060e8e51577
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-14 13:39:21 -0300

    Remove RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
-  resources :users
-  post "/graphql", to: "graphql#execute"
-  resources :recipes
-  resources :materials
+  post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
