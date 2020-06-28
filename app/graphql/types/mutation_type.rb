633db76778b0b88eb75012f072e003aed1d7cfb2
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-28 18:50:28 -0300

    Remove mutation to create user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_user, mutation: Mutations::CreateUser
     field :update_user, mutation: Mutations::UpdateUser
     field :create_material, mutation: Mutations::CreateMaterial
     field :update_material, mutation: Mutations::UpdateMaterial
